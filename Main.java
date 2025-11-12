556d139a653c7693705b7504b787f0ddd0d5630a
Rehaf912 <mousreha@kkg-do.eu>
2025-11-12 16:34:53 +0100

    Add multiple Employee instances and display departments
    Added multiple Employee instances and demonstrated department changes.

--- Main.java
+++ Main.java
@@ -1,5 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Employee arbeiter1 = new Employee("Klaus", "Herny", 29031980, "männlich", 45, 74, "Herr Klaus ist immer unpünktlich");
@@ -10,5 +8,17 @@ public class Main {
         arbeiter1.setNewAbteilung("C64");
         arbeiter1.zeigeAbteilungAn();
 
+        Employee e1 = new Employee("Klaus1", "Henry", 29031980, "männlich", 45, 74, "Herr Klaus ist immer unpünktlich");
+        Employee e2 = new Employee("Klaus2", "Henry", 29031980, "männlich", 45, 74, "Herr Klaus ist immer unpünktlich");
+        Employee e3 = new Employee("Klaus3", "Henry", 29031980, "männlich", 45, 74, "Herr Klaus ist immer unpünktlich");
+        Employee e4 = e1;
+
+        e1.setNewAbteilung("C64");
+        e2.setNewAbteilung("C64");
+
+        System.out.print("e1: "); e1.zeigeAbteilungAn();
+        System.out.print("e2: "); e2.zeigeAbteilungAn();
+        System.out.print("e3: "); e3.zeigeAbteilungAn();
+        System.out.print("e4: "); e4.zeigeAbteilungAn();
     }
 }
